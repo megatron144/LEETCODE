1c54a5d89053eba2761e898c49b1cae8b2cef2dd
Aditya Raj <152900366+Megatron144@users.noreply.github.com>
2025-03-14 10:30:28 +0530

    Time: 107 ms (5.02%), Space: 91.5 MB (5.02%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,29 @@
+class Solution {
+#define allR(v) v.rbegin(),v.rend()
+private:
+    bool f(vector<int>& candies,long long mid, long long k){
+        for(auto x: candies){
+            k-=(long long)x/mid;
+            if(k<=0)return true;
+        }
+        return false;
+    }
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        sort(allR(candies));
+        long long tot=accumulate(allR(candies),(long long)0);
+        int n=candies.size();
+        if(tot<k)return 0;
+        tot/=k;
+        long long l=1,h=(long long)candies[n-1]>=tot?candies[n-1]:tot,mid,ans;
+        while(l<=h){
+            mid=l+(h-l)/2;
+            if(f(candies,mid,k)){
+                ans=mid;
+                l=mid+1;
+            }
+            else h=mid-1;
+        }
+        return (int)ans;
+    }
+};
