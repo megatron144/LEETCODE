a5be0facd828806a7485f442b2663f913b036042
Aditya Raj <152900366+megatron144@users.noreply.github.com>
2026-09-11 11:33:44 +0530

    Time: 5 ms (42.83%), Space: 8.4 MB (87.34%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -3,16 +3,18 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int n=s1.length(),m=s2.length(),nm=s3.length();
         if(n+m!=nm)return false;
-        vector<vector<bool>> dp(n+1,vector<bool> (m+1,false));
+        vector<bool> prev(m+1,false);
         for(int i=0;i<=n;i++){
+            vector<bool> curr(m+1,false);
             for(int j=0;j<=m;j++){
-                if(!i && !j)dp[i][j]=true;
-                else if(!i)dp[i][j]=(dp[i][j-1] && s2[j-1]==s3[i+j-1]);
-                else if(!j)dp[i][j]=(dp[i-1][j] && s1[i-1]==s3[i+j-1]);
-                else dp[i][j]=(dp[i-1][j] && s1[i-1]==s3[i+j-1]) | 
-                                (dp[i][j-1] && s2[j-1]==s3[i+j-1]);
+                if(!i && !j)curr[j]=true;
+                else if(!i)curr[j]=(curr[j-1] && s2[j-1]==s3[i+j-1]);
+                else if(!j)curr[j]=(prev[j] && s1[i-1]==s3[i+j-1]);
+                else curr[j]=(prev[j] && s1[i-1]==s3[i+j-1]) | 
+                                (curr[j-1] && s2[j-1]==s3[i+j-1]);
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
