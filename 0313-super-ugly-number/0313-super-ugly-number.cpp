6f6c6e38d05f4c6741db82adbd4ee936d3b96cea
Aditya Raj <152900366+megatron144@users.noreply.github.com>
2026-09-15 23:03:00 +0530

    Time: 99 ms (48.1%), Space: 13.9 MB (48.03%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.cpp
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        int cnt=0;
-        long long val=0;
-        priority_queue<long long,vector<long long>,greater<long long>> pq;
-        pq.push(1);
-        int i=0;
-        while(!pq.empty() && cnt!=n){
-            long long mx=pq.top();
-            val=mx;
-            for(int j=0;j<primes.size();j++){
-               pq.push(mx*primes[j]);
-            }
-            while(!pq.empty() && mx==pq.top()){
-               pq.pop();
-            }
-            cnt++;
+        // int cnt=0;
+        // long long val=0;
+        // priority_queue<long long,vector<long long>,greater<long long>> pq;
+        // pq.push(1);
+        // int i=0;
+        // while(!pq.empty() && cnt!=n){
+        //     long long mx=pq.top();
+        //     val=mx;
+        //     for(int j=0;j<primes.size();j++){
+        //        pq.push(mx*primes[j]);
+        //     }
+        //     while(!pq.empty() && mx==pq.top()){
+        //        pq.pop();
+        //     }
+        //     cnt++;
+        // }
+        // return val;
+        vector<long long> dp(n);
+        int k=primes.size();
+        vector<int> idx(k,0);
+        dp[0]=1;
+        for(int i=1;i<n;++i){
+            long long mn=INT_MAX;
+            for(int j=0;j<k;++j)mn=min(mn,dp[idx[j]]*primes[j]);
+            dp[i]=mn;
+            for(int j=0;j<k;++j)if(mn==(dp[idx[j]]*primes[j]))idx[j]++;
         }
-        return val;
+        return dp[n-1];
     }
 };
