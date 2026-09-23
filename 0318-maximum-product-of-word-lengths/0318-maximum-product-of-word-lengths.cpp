2ee49cb8644be0096c5140e450a4ba0f4bbc9ff7
Aditya Raj <152900366+megatron144@users.noreply.github.com>
2026-09-23 21:17:04 +0530

    Time: 7 ms (67.74%), Space: 26 MB (66.01%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        vector<vector<int>> v(26);
+        // vector<vector<int>> v(26);
         int n=words.size();
-        for(int i=0;i<n;i++){
-            string x=words[i];
-            vector<int> pres(26,0);
-            for(auto& ch: x){
-                if(!pres[ch-'a'])v[ch-'a'].push_back(i),pres[ch-'a']=1;
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     string x=words[i];
+        //     vector<int> pres(26,0);
+        //     for(auto& ch: x){
+        //         if(!pres[ch-'a'])v[ch-'a'].push_back(i),pres[ch-'a']=1;
+        //     }
+        // }
         int mx=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         bool flg=true;
+        //         for(int k=0;k<26;k++){
+        //             if(binary_search(v[k].begin(),v[k].end(),i) &&
+        //                     binary_search(v[k].begin(),v[k].end(),j)){
+        //                 flg=false;
+        //                 break;
+        //             }
+        //         }
+        //         if(flg)mx=max(mx,(int)words[i].size()*(int)words[j].size());
+        //     }
+        // }
+        vector<bitset<26>> v(n);
+        for(int i=0;i<n;i++)
+            for(char ch:words[i])
+                v[i][ch-'a']=1;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                bool flg=true;
-                for(int k=0;k<26;k++){
-                    if(binary_search(v[k].begin(),v[k].end(),i) &&
-                            binary_search(v[k].begin(),v[k].end(),j)){
-                        flg=false;
-                        break;
-                    }
+                if((v[i] & v[j]).none()){
+                    mx=max(mx,(int)words[i].size()*(int)words[j].size());
                 }
-                if(flg)mx=max(mx,(int)words[i].size()*(int)words[j].size());
             }
         }
         return mx;
