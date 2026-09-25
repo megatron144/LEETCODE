8f02748d45892e6bf4034bc90ff465f30069f626
Aditya Raj <152900366+megatron144@users.noreply.github.com>
2026-09-25 23:45:18 +0530

    Time: 2 ms (8.02%), Space: 8.6 MB (12.86%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,38 +1,39 @@
 class Solution {
-// private:
-//     bool check(int& x,int& y,int currx,int curry,int& target){
-//         if((currx+curry)==target)return true;
-//         if(!currx && curry){
-//             bool flg=check(x,y,x,curry,target);
-//             //y->x
-//             if(curry>=x)flg|=check(x,y,x,curry-x,target);
-//             else flg|=check(x,y,curry,0,target);
-//             return flg;
-//         }
-//         if(!curry && currx){
-//             bool flg=check(x,y,currx,y,target);
-//             //x->y
-//             if(currx>=y)flg|=check(x,y,currx-y,y,target);
-//             else flg|=check(x,y,0,currx,target);
-//             return flg;
-//         }
-//         bool flg=check(x,y,x,curry,target)|check(x,y,currx,y,target);
-//         //x->y
-//         {
-//             if((currx+curry)<=y)flg|=check(x,y,0,currx+curry,target);
-//             else flg|=check(x,y,currx-(y-curry),y,target);
-//         }
-//         //y->x
-//         {
-//             if((currx+curry)<=x)flg|=check(x,y,currx+curry,0,target);
-//             else flg|=check(x,y,x,curry-(x-currx),target);
-//         }
-//         return flg;
-//     }
+    vector<vector<bool>> dp;
+private:
+    bool check(int x, int y, int currx, int curry, int target) {
+        if (currx + curry == target || currx == target || curry == target) 
+            return true;
+
+        if (dp[currx][curry]) 
+            return false;
+        
+        dp[currx][curry] = true;
+
+        // 1. Fill X
+        if (check(x, y, x, curry, target)) return true;
+        // 2. Fill Y
+        if (check(x, y, currx, y, target)) return true;
+        // 3. Empty X
+        if (check(x, y, 0, curry, target)) return true;
+        // 4. Empty Y
+        if (check(x, y, currx, 0, target)) return true;
+
+        // 5. Pour X -> Y
+        int pourXtoY = std::min(currx, y - curry);
+        if (check(x, y, currx - pourXtoY, curry + pourXtoY, target)) return true;
+
+        // 6. Pour Y -> X
+        int pourYtoX = std::min(curry, x - currx);
+        if (check(x, y, currx + pourYtoX, curry - pourYtoX, target)) return true;
+        return false;
+    }
 public:
     bool canMeasureWater(int x, int y, int target) {
         if((x+y)==target)return true;
         if((x+y)<target)return false;
-        return !(target%gcd(x,y));
+        dp.assign(x+1,vector<bool> (y+1,false));
+        return check(x,y,0,0,target);
+        //return !(target%gcd(x,y));
     }
 };
